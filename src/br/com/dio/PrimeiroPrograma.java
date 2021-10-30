26c2531dc1918128f5dc96b50e332319c675bfd5
Contreira-C <caioperes2001@gmail.com>
2021-10-30 11:08:45 -0300

    .

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,8 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args){
+        int a = 1, b = 5;
+        System.out.println("Hello World! " + (a + b));
+    }
+}
